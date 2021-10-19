3576e3f886388a13dfd6bb0cccf61154feeee387
Carlos <gohercarlos@gmail.com>
2021-10-18 23:37:20 -0500

    Cambio en el formulario de usuario

--- frontend/src/store/index.js
+++ frontend/src/store/index.js
@@ -5,6 +5,24 @@ Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
+    list_country: [],
+    list_document: [],
+    list_area: [],
+    list_user: []
+  },
+  getters: {
+    getCountries: (state) => {
+      return state.list_country
+    },
+    getDocuments: (state) => {
+      return state.list_document
+    },
+    getAreas: (state) => {
+      return state.list_area
+    },
+    getUsers: (state) => {
+      return state.list_user
+    }
   },
   mutations: {
   },
--- frontend/src/views/RegisterUser.vue
+++ frontend/src/views/RegisterUser.vue
@@ -6,65 +6,77 @@
         <div class="form-row">
             <div class="form-group col-md-6">
                 <label>Tipo de documento</label>
-                <select class="form-control" id="exampleFormControlSelect1">
-                    <option>1</option>
-                    <option>2</option>
-                    <option>3</option>
-                    <option>4</option>
-                    <option>5</option>
+                <select class="form-control" v-model="user.document_type">
+                    <option v-for="(item, index) in this.getDocuments" :key="index">2</option>
                 </select>
             </div>
             <div class="form-group col-md-6">
                 <label>Numero de documento</label>
-                <input type="text" class="form-control">
+                <input type="text" class="form-control" v-model="user.document">
             </div>
         </div>
         <div class="form-row">
             <div class="form-group col-md-6">
                 <label>Primer nombre</label>
-                <input type="text" class="form-control">
+                <input type="text" class="form-control" v-model="user.first_name">
             </div>
             <div class="form-group col-md-6">
                 <label>Segundo nombre</label>
-                <input type="text" class="form-control">
+                <input type="text" class="form-control" v-model="user.second_name">
             </div>
         </div>
         <div class="form-row">
             <div class="form-group col-md-6">
                 <label>Primer apellido</label>
-                <input type="text" class="form-control">
+                <input type="text" class="form-control" v-model="user.surname">
             </div>
             <div class="form-group col-md-6">
                 <label>Segundo apellido</label>
-                <input type="text" class="form-control">
+                <input type="text" class="form-control" v-model="user.second_surname">
             </div>
         </div>
         <div class="form-row">
             <div class="form-group col-md-4">
                 <label>Pais</label>
-                <select class="form-control" id="exampleFormControlSelect1">
-                    <option>1</option>
-                    <option>2</option>
-                    <option>3</option>
-                    <option>4</option>
-                    <option>5</option>
+                <select class="form-control" v-model="user.country">
+                    <option v-for="(item, index) in this.getCountries" :key="index+'country'">2</option>
                 </select>
             </div>
             <div class="form-group col-md-4">
                 <label>Area</label>
-                <select class="form-control" id="exampleFormControlSelect1">
-                    <option>1</option>
-                    <option>2</option>
-                    <option>3</option>
-                    <option>4</option>
-                    <option>5</option>
+                <select class="form-control" v-model="user.area">
+                    <option v-for="(item, index) in this.getAreas" :key="index+'area'">2</option>
                 </select>
             </div>
             <div class="form-group col-md-4">
                 <label>Fecha de ingreso</label>
-                <input type="date" class="form-control">
+                <input type="date" class="form-control" v-model="user.date_admission">
             </div>
         </div>
         <button type="submit" class="btn btn-primary">Guardar</button>
     </form>
 </template>
+
+<script>
+import {mapGetters} from 'vuex'
+export default {
+    data () {
+        return {
+            user:{
+                document_type: '',
+                document: '',
+                first_name: '',
+                second_name: '',
+                surname: '',
+                second_surname: '',
+                country: '',
+                area: '',
+                date_admission: ''
+            }
+        }
+    },
+    computed: {
+        ...mapGetters(['getCountries', 'getDocuments', 'getAreas'])
+    }    
+}
+</script>
